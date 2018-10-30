29a1581d9285ffbf436417a68805870564433826
Russell Brant-Gargan <brantgarganrussell@gmail.com>
2018-10-30 16:40:12 -0400

    Update functions/Sports/sportsinfo.js
    Updated sportsinfo.js with more information for each event

--- functions/Sports/sportsinfo.js
+++ functions/Sports/sportsinfo.js
@@ -1,21 +1,34 @@
 let Parser = require('rss-parser');
-let parser = new Parser();
 
-
-
-(async () => {
-
-  let feed = await parser.parseURL('https://gopsusports.com/calendar.ashx/calendar.rss?sport_id=0');
-  console.log(feed.title);
-
-  feed.items.forEach(item => {
-    console.log(item)
-  });
-
+let parser = new Parser({
+  customFields: {
+    item: [
+      ['ev:location', 'location'],
+      ['ev:startdate', 'startdate'],
+      ['ev:enddate', 'enddate'],
+      ['s:localstartdate', 'localstartdate'],
+      ['s:localenddate', 'localenddate'],
+      ['s:teamlogo', 'teamlogo'],
+      ['s:opponentlogo', 'oponentlogo'],
+      ['s:opponent', 'opponent'],
+      ['s:gameid', 'gameid'],
+      ['s:gamepromoname', 'gamepromoname'],
+      ['s:links', 'links']
+    ],
+  }
+});
 /*
-  feed.items.forEach(item => {
-    console.log("{" + item.title + "}")
-  });
+    other variable information
+    link
+    content
+    contentsnippet
+    guid
 */
 
-})();
+parser.parseURL('https://gopsusports.com/calendar.ashx/calendar.rss?sport_id=0', function(err, feed) {
+  console.log(feed.title);
+
+  feed.items.forEach(function(entry) {
+    console.log("{" + entry.title + ", will be located at " + entry.location + ", will start on " + entry.startdate + ", will end on " + entry.enddate + ", the opponent will be " + entry.opponent + "}");
+  })
+})
