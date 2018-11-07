ab576e6bdd31019311af0ce486acfa9162c45e60
WoodruffA122 <woodruff.r.alexander@gmail.com>
2018-11-06 19:43:20 -0500

    set up basic file config

--- functions/Academic Calender/academic_calender_logic.js
+++ functions/Academic Calender/academic_calender_logic.js
@@ -1,1 +1,14 @@
-tes
+//Gets year Mappings in JSON
+const = yearMappings = require("./year_mappings");
+
+//Class containing functions for specific calender queries
+class academicCalenderService{
+	async getLateDrop(term, year){
+		//returns something like string in final response
+		//append term and year into string
+		//pass to python script
+		//get JSON back
+		//parse JSON for answer
+		//return answer
+	}
+}
