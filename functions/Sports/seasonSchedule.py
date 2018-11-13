54268b3ebf70e0704aa9fed1a16be41d3259c847
Russell Brant-Gargan <brantgarganrussell@gmail.com>
2018-11-13 18:16:48 -0500

    Updated Sports info with season, and upcoming home game.
    Added seasonSchedule.py, and upcomingHomeGame.py, these show the season for a sport and the nearest upcoming home game

---
+++ functions/Sports/seasonSchedule.py
@@ -0,0 +1,128 @@
+import sys, json, nltk, datetime
+
+co_sports = ['Football', 'Basketball', 'Track/Field', 'Fencing'] #non gender specific sports
+gen_sports = ['Wrestling', 'Golf', 'Lacrosse', 'Tennis', 'Gymnastics', 'Volleyball', 'Swimming', 'Ice', 'Basketball'] #gender specific sports
+
+date = str(datetime.datetime.now())[:10]
+
+def read_in():
+    lines = sys.stdin.readlines()
+    #Since our input would be on the first line, parse our JSON data from that
+    input = json.loads(lines[0])
+
+    return input
+
+def main():
+    #get our data as an array from read_in()
+    lines = read_in()
+
+    sport = nltk.word_tokenize(sys.argv[1])
+
+    year = sys.argv[2]
+
+    gender = ""
+
+    for s in gen_sports: #This is in case the gender is not provided
+        if sport[0] == s:
+            for e in lines:
+                for a in e:
+                    dict = {}
+                    dict = e[a]
+                    if str(dict["startdate"])[:4] < date[:4]:
+                        continue
+                    if str(dict["startdate"])[:4] >= date[:4]:
+                        if str(dict["startdate"])[5:7] < date[5:7] and str(dict["startdate"])[:4] == date[:4]:
+                            continue
+                    if str(dict["startdate"])[:4] >= date[:4]:
+                        if str(dict["startdate"])[5:7] >= date[5:7]:
+                            if str(dict["startdate"])[8:10] < date[8:10] and str(dict["startdate"])[:4] == date[:4]:
+                                continue
+
+                    title = nltk.word_tokenize(dict["title"])
+                    for z in title:
+                        if z == s:
+                            if str(dict["startdate"])[:4] == str(year):
+                                if int(str(dict["startdate"])[5:7]) > 6:
+                                    print(dict["title"])
+                                    print(dict["startdate"])
+                            elif str(dict["startdate"])[:4] == str(int(str(year)) + 1):
+                                if int(str(dict["startdate"])[5:7]) <= 6:
+                                    print(dict["title"])
+                                    print(dict["startdate"])
+                            else:
+                                continue
+
+    if len(sport) > 1: #this is in case the gender is provided
+        if sport[0] == "Men":
+            gender = "Men"
+        if sport[0] == "Women":
+            gender = "Women"
+        for s in gen_sports:
+            if sport[2] == s:
+                for e in lines:
+                    for a in e:
+                        dict = {}
+                        dict = e[a]
+                        if str(dict["startdate"])[:4] < date[:4]:
+                            continue
+                        if str(dict["startdate"])[:4] >= date[:4]:
+                            if str(dict["startdate"])[5:7] < date[5:7] and str(dict["startdate"])[:4] == date[:4]:
+                                continue
+                        if str(dict["startdate"])[:4] >= date[:4]:
+                            if str(dict["startdate"])[5:7] >= date[5:7]:
+                                if str(dict["startdate"])[8:10] < date[8:10] and str(dict["startdate"])[:4] == date[:4]:
+                                    continue
+
+                        title = nltk.word_tokenize(dict["title"])
+                        gender_correct = False
+                        for z in title:
+                            if z == gender:
+                                gender_correct = True
+                                continue
+                            if gender_correct:
+                                if z == s:
+                                    if str(dict["startdate"])[:4] == str(year):
+                                        if int(str(dict["startdate"])[5:7]) > 6:
+                                            print(dict["title"])
+                                            print(dict["startdate"])
+                                    elif str(dict["startdate"])[:4] == str(int(str(year)) + 1):
+                                        if int(str(dict["startdate"])[5:7]) <= 6:
+                                            print(dict["title"])
+                                            print(dict["startdate"])
+                                    else:
+                                        continue
+
+    if len(sport) == 1: #This is in case the sport is not gender specific
+        for s in co_sports:
+            if sport[0] == s:
+                for e in lines:
+                    for a in e:
+                        dict = {}
+                        dict = e[a]
+                        if str(dict["startdate"])[:4] < date[:4]:
+                            continue
+                        if str(dict["startdate"])[:4] >= date[:4]:
+                            if str(dict["startdate"])[5:7] < date[5:7] and str(dict["startdate"])[:4] == date[:4]:
+                                continue
+                        if str(dict["startdate"])[:4] >= date[:4]:
+                            if str(dict["startdate"])[5:7] >= date[5:7]:
+                                if str(dict["startdate"])[8:10] < date[8:10] and str(dict["startdate"])[:4] == date[:4]:
+                                    continue
+
+                        title = nltk.word_tokenize(dict["title"])
+                        for z in title:
+                            if z == s:
+                                print(str(dict["startdate"])[:4])
+                                if str(dict["startdate"])[:4] == str(year):
+                                    if int(str(dict["startdate"])[5:7]) > 6:
+                                        print(dict["title"])
+                                        print(dict["startdate"])
+                                elif str(dict["startdate"])[:4] == str(int(str(year)) + 1):
+                                    if int(str(dict["startdate"])[5:7]) <= 6:
+                                        print(dict["title"])
+                                        print(dict["startdate"])
+                                else:
+                                    continue
+
+if __name__ == '__main__':
+    main()
--- functions/Sports/sportsinfo.js
+++ functions/Sports/sportsinfo.js
@@ -44,7 +44,59 @@ parser.parseURL('https://gopsusports.com/calendar.ashx/calendar.rss?sport_id=0',
   })
 })
 
+exports.upcomingHomeGame = function pyHome(sport){
 
+let dataList = "The next " + sport + " home game is: ";
+
+data = fullList;
+
+var spawn = require('child_process').spawn,
+py    = spawn('python', ['upcomingHomeGame.py', sport]),
+data,
+dataString = '';
+
+/*Here we are saying that every time our node application receives data from the python process output stream(on 'data'), we want to convert that received data into a string and append it to the overall dataString.*/
+py.stdout.on('data', function(data){
+  dataList += data.toString();
+});
+
+/*Once the stream is done (on 'end') we want to simply log the received data to the console.*/
+py.stdout.on('end', function(){
+  console.log(dataList);
+});
+
+/*We have to stringify the data first otherwise our python process wont recognize it*/
+py.stdin.write(JSON.stringify(fullList));
+
+py.stdin.end();
+}
+
+exports.seasonSchedule function pySeason(sport, year){
+
+let dataList = "The " + sport + " Season: \n";
+
+data = fullList;
+
+var spawn = require('child_process').spawn,
+py    = spawn('python', ['seasonSchedule.py', sport, year]),
+data,
+dataString = '';
+
+/*Here we are saying that every time our node application receives data from the python process output stream(on 'data'), we want to convert that received data into a string and append it to the overall dataString.*/
+py.stdout.on('data', function(data){
+  dataList += data.toString();
+});
+
+/*Once the stream is done (on 'end') we want to simply log the received data to the console.*/
+py.stdout.on('end', function(){
+  console.log(dataList);
+});
+
+/*We have to stringify the data first otherwise our python process wont recognize it*/
+py.stdin.write(JSON.stringify(fullList));
+
+py.stdin.end();
+}
 
 exports.upcomingGame = function pyResult(sport){
 
