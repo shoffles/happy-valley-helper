64f25c2ddfe3dae67894727295be7cfb921aed65
WoodruffA122 <woodruff.r.alexander@gmail.com>
2018-11-13 19:38:00 -0500

    Added default logic for academic calendar

--- functions/Academic Calender/academic_calender_logic.js
+++ functions/Academic Calender/academic_calender_logic.js
@@ -4,6 +4,37 @@ const = yearMappings = require("./year_mappings");
 //Class containing functions for specific calender queries
 class academicCalenderService{
 	async getLateDrop(term, year){
+		var date = new Date();
+		console.log(month, year)
+
+		if(term == null){
+			month = date.getMonth()
+
+			if(month >= 9 && month <=12){
+				term = "fall"
+				//console.log("fall")
+
+			}
+		  	else if (month >= 1 && month <= 5) {
+		  		term = "spring"
+		  		//console.log("spring")
+			}
+			else {
+				term = "summer"
+				//console.log("summer")
+			}
+		}
+
+		if(year == null){
+			yearDig = date.getFullYear()
+			year = yearDig
+			//console.log(year)
+		}
+
+
+
+		//call python script
+    	// load here
 		//hardcode latedrop "2Late Drop Begins"
 		//returns something like string in final response
 		//append term and year into string
