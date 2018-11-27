cd69fd2095173abb605148c7c7e4ddcd7622f02f
Russell Brant-Gargan <brantgarganrussell@gmail.com>
2018-11-27 17:14:51 -0500

    Results of Game
    Putting in the final gameresult search, next on my agenda is intents

--- functions/Sports/sportsinfo.js
+++ functions/Sports/sportsinfo.js
@@ -44,6 +44,36 @@ parser.parseURL('https://gopsusports.com/calendar.ashx/calendar.rss?sport_id=0',
   })
 })
 
+exports.gameResult = function pyScore(sport, date){
+
+  let dataList = "";
+
+  data = fullList;
+
+  var spawn = require('child_process').spawn,
+  py    = spawn('python', ['gameResult.py', sport, date]),
+  data,
+  dataString = '';
+
+  /*Here we are saying that every time our node application receives data from the python process output stream(on 'data'), we want to convert that received data into a string and append it to the overall dataString.*/
+  py.stdout.on('data', function(data){
+    dataList += data.toString();
+  });
+
+  /*Once the stream is done (on 'end') we want to simply log the received data to the console.*/
+  py.stdout.on('end', function(){
+    console.log(dataList);
+  });
+
+  /*We have to stringify the data first otherwise our python process wont recognize it*/
+  py.stdin.write(JSON.stringify(fullList));
+
+  py.stdin.end();
+
+  return dataList;
+
+}
+
 exports.seasonNumLeft = function pySeasonNum(sport, year){
 
 let dataList = "The " + sport + " season has ";
